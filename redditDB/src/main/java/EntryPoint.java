3cc08843f85eb1c8d6a55374982377572668b52b
Jacob Skoog <jacob.skoog@protonmail.com>
2021-12-30 14:57:02 +0100

    code connected in test

--- redditDB/src/main/java/EntryPoint.java
+++ redditDB/src/main/java/EntryPoint.java
@@ -4,13 +4,19 @@ import java.sql.*;
 
 public class EntryPoint {
     public static void main(String[] args) throws SQLException {
-        try (Connection conn = DriverManager.getConnection(CONNINFO.URL.toString(), CONNINFO.URL.toString(), CONNINFO.PASSWORD.toString())) {
+        try (Connection conn = DriverManager.getConnection(CONNINFO.URL.toString(), CONNINFO.USERNAME.toString(), CONNINFO.PASSWORD.toString())) {
+            conn.setCatalog("test");
             try (Statement stmt = conn.createStatement()) {
-                try (ResultSet rs = stmt.executeQuery("USE `test` SELECT `ID`")) {
+                try (ResultSet rs = stmt.executeQuery("SELECT ID FROM classes")) {
                     rs.first();
                     System.out.println(rs.getString(1));
+                    while (rs.next()) {
+                        System.out.println(rs.getString(1));
+                    }
                 }
             }
         }
+
+
     }
 }
