28c1abef2fcf6476649f0690d6dc6949de4dc8ca
Jacob Skoog <jacob.skoog@protonmail.com>
2022-01-30 22:21:34 +0100

    started writing main program to run the tests.

--- redditDB/src/main/java/EntryPoint.java
+++ redditDB/src/main/java/EntryPoint.java
@@ -4,6 +4,7 @@ import comments.FullComment;
 import db.accessors.*;
 import files.readers.RedditJSONExtractor;
 import io.github.cdimascio.dotenv.Dotenv;
+import time.test.run.Tester;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,57 +12,15 @@ import java.sql.*;
 import java.util.LinkedList;
 
 public class EntryPoint {
-    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+    public static void main(String[] args) {
         Dotenv env = Dotenv.load();
-        try {
-            // Creating extractor to extract JSON objects from the file
-            RedditJSONExtractor extractor = new RedditJSONExtractor(args[0]);
-            // Creating a base connection to the SQL DB
-            final SQLAccessParams params = new SQLAccessParams(env.get("SQL_URL"), env.get("SQL_UNAME"), env.get("SQL_PWORD"));
-            final Connection conn = params.getConnection();
-            // Creating the schema and required tables if they don't exist
-            final String schemaName = "test_db";
-//            SchemaOperations.createSchema(conn, schemaName);
-            SQLTableManager.createUnconstrainedTables(conn, schemaName);
-            SQLTableManager.createConstrainedTables(conn, schemaName);
-            boolean constrained = false;
-            // Creating moshi adapter to read in JSON to FullComment objects
-            Moshi moshi = new Moshi.Builder().build();
-            JsonAdapter<FullComment> adapter = moshi.adapter(FullComment.class);
-            LinkedList<FullComment> list = new LinkedList<>();
-
-            // Reading JSON from the file. If 1000 objects have been read in,
-            int batchSize = constrained ? 10000 : 1000; // Constrained can have larger batches as it is only processing one batch at a time
-            int coreCount = Runtime.getRuntime().availableProcessors();
-            LinkedList<Thread> threads = new LinkedList<>();
-            while (extractor.hasNext()) {
-                String json = extractor.extractJSONObject();
-                list.add(adapter.fromJson(json));
-                if (list.size() == batchSize || !extractor.hasNext()) {
-                    FullComment[] comments = new FullComment[list.size()];
-                    list.toArray(comments);
-                    list.clear();
-                    Runnable loader = constrained ? new DBLoaderConstrained(params, schemaName, comments) : new DBLoaderUnconstrained(params, schemaName, comments);
-                    if (constrained) {
-                        loader.run();
-                    } else {
-                        Thread t = new Thread(loader);
-                        t.start();
-                        threads.add(t);
-                        if (threads.size() == coreCount) {
-                            threads.getFirst().join();
-                            threads.removeFirst();
-                        }
-                    }
-                }
-            }
-            for (Thread t : threads) try { t.join(); } catch (InterruptedException ignore) { }
-            LoadAfterStaging loader = new LoadAfterStaging(params, schemaName);
-            loader.load();
-        } catch (FileNotFoundException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
+        SQLAccessParams params = new SQLAccessParams(
+                env.get("SQL_URL"),
+                env.get("SQL_UNAME"),
+                env.get("SQL_PWORD")
+        );
+        String srcFile = args[0];
+        assert srcFile != null && !srcFile.equals("");
+        Tester tester = new Tester(params, srcFile, "test_db");
     }
 }
